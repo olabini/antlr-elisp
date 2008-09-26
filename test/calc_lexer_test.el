850ad4cbd34e16922adf4fa22cd2fb326d6f8b33
Ola Bini <ola.bini@gmail.com>
2008-09-26 13:36:35 -0700

    Add another test for numbers

--- test/calc_lexer_test.el
+++ test/calc_lexer_test.el
@@ -4,4 +4,9 @@
       (assert-equal "Basic match"
                     '((9 . "123") ; 9 == integer
                       (-1 . nil))
-                    (collect-lex-tokens 'calc_elispLexer "123")))
+                    (collect-lex-tokens 'calc_elispLexer "123"))
+      (assert-equal "Basic match2"
+                    '((9 . "321") ; 9 == integer
+                      (-1 . nil))
+                    (collect-lex-tokens 'calc_elispLexer "321"))
+      )
